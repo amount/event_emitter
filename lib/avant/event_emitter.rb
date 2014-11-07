9666ee97b07569f7611ceef176daf428676d2010
Nathan Keyes <nathan.keyes@avantcredit.com>
2014-11-07 16:05:58 -0600

    first commit

---
+++ lib/avant/event_emitter.rb
@@ -0,0 +1,7 @@
+require "avant/event_emitter/version"
+
+module Avant
+  module EventEmitter
+    # Your code goes here...
+  end
+end
---
+++ lib/avant/event_emitter/version.rb
@@ -0,0 +1,5 @@
+module Avant
+  module EventEmitter
+    VERSION = "0.0.1"
+  end
+end
