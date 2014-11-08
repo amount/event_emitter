80736e996a857bd80fb1281e75e89bb2405c818e
Nathan Keyes <nathan.keyes@avantcredit.com>
2014-11-07 18:58:07 -0600

    actually emit stats [#82322336]

--- lib/avant/event_emitter/multiplexer.rb
+++ lib/avant/event_emitter/multiplexer.rb
@@ -19,6 +19,14 @@ module Avant
       def subscribe
         Philotic::Subscriber.subscribe(SUBSCRIPTION) do |metadata, message|
           ap message: message, metadata: metadata
+          stat = {
+              'stat' => message[:attributes]['stat'],
+              'count' => message[:attributes]['count'],
+              't' => metadata[:timestamp],
+
+          }
+
+          emit!(stat)
         end
         while true
           sleep 1
