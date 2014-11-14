62baa12034783365739c3df9c68bb02f1c772994
Nathan Keyes <nathan.keyes@avantcredit.com>
2014-11-13 18:47:34 -0600

    clean up event [#82322336]

--- lib/avant/event_emitter/event.rb
+++ lib/avant/event_emitter/event.rb
@@ -4,9 +4,9 @@ module Avant
   module EventEmitter
     class Event < Philotic::Event
 
-      PHILOTIC_PRODUCT = 'avant'.freeze
-      PHILOTIC_COMPONENT = 'event_emitter'.freeze
-      PHILOTIC_EVENT_TYPE = 'event_emitter.event'.freeze
+      PRODUCT    = :avant
+      COMPONENT  = :event_emitter
+      EVENT_TYPE = :'event_emitter.event'
 
       attr_routable :stat
       attr_payload :count
@@ -15,25 +15,22 @@ module Avant
       def initialize(attributes={})
         super
 
-        self.philotic_product = PHILOTIC_PRODUCT
-        self.philotic_component = PHILOTIC_COMPONENT
-        self.philotic_event_type = PHILOTIC_EVENT_TYPE
+        @philotic_product    = PRODUCT
+        @philotic_component  = COMPONENT
+        @philotic_event_type = EVENT_TYPE
 
-        self.stat = attributes['stat']
+        self.stat  = attributes['stat']
         self.count = attributes['count']
         self.value = attributes['value']
       end
 
       def philotic_product=(val)
-        super PHILOTIC_PRODUCT
       end
 
       def philotic_component=(val)
-        super PHILOTIC_COMPONENT
       end
 
       def philotic_event_type=(val)
-        super PHILOTIC_EVENT_TYPE
       end
 
     end
