0880716e8970827389b4a2123d8e88e0327a2b9b
Nathan Keyes <nathan.keyes@avantcredit.com>
2014-12-17 10:18:25 -0600

    expose philotic queues

--- lib/avant/event_emitter.rb
+++ lib/avant/event_emitter.rb
@@ -1,10 +1,17 @@
 Dir["#{File.dirname(__FILE__)}/#{File.basename(__FILE__, '.rb')}/*.rb"].each {|file| require file }
+require 'pathname'
+require 'yaml'
 
 # Emits statsd style events to a set of services.
 #
 module Avant
   module EventEmitter
-
+    def self.root
+      ::Pathname.new File.expand_path('../../../', __FILE__)
+    end
+    def self.philotic_queues
+      YAML.load_file(root.join('config', 'philotic_queues.yml'))
+    end
   end
 end
 
