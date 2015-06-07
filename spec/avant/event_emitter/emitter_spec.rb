b214e2cd9dcdf10c266c1abe958fbeb8e54a2378
Nathan Keyes <nathan.keyes@avantcredit.com>
2015-06-07 12:20:02 -0500

    fix test [#96394906]

--- spec/avant/event_emitter/emitter_spec.rb
+++ spec/avant/event_emitter/emitter_spec.rb
@@ -8,8 +8,8 @@ module Avant
 
       describe '.emitters' do
         it 'should find all emitters' do
-          ENV['HOSTED_GRAPHITE_EMITTER_ENABLED'] = '1'
-          ENV['STATHAT_EMITTER_ENABLED'] = '1'
+          expect(ENV).to receive(:[]).with('HOSTED_GRAPHITE_EMITTER_ENABLED').and_return('1')
+          expect(ENV).to receive(:[]).with('STAT_HAT_EMITTER_ENABLED').and_return('1')
           expect(subject.emitters).to include(Avant::EventEmitter::Emitter::StatHatEmitter, Avant::EventEmitter::Emitter::HostedGraphiteEmitter)
         end
       end
